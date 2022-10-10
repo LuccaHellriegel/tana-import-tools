5cc75b7a7da1586dd8ea732d583df5f00033e472
vicrdguez <vicrerdgz@gmail.com>
2022-10-10 22:11:58 +0300

    additional merge conflicts

--- src/converters/obsidian/createFileNode.ts
+++ src/converters/obsidian/createFileNode.ts
@@ -1,6 +1,7 @@
-import { TanaIntermediateNode } from '../../types/types';
+import { NodeType, TanaIntermediateNode } from '../../types/types';
 import { FrontmatterData } from './parseFrontmatter';
 import { UidRequestType, VaultContext } from './VaultContext';
+import moment from 'moment'
 
 export function createFileNode(
   displayName: string,
@@ -16,12 +17,31 @@ export function createFileNode(
     }
   });
 
+  let nodeUid = context.uidRequest(displayName, UidRequestType.FILE);
+  let nodeType: NodeType = 'node';
+  const dateDisplayName =  dateStringToDateUID(displayName, context.dailyNoteFormat);
+
+  if (dateDisplayName.length > 0) {
+    nodeUid = dateDisplayName;
+    nodeType = 'date';
+    displayName = dateDisplayName;
+  }
+
   return {
-    uid: context.uidRequest(displayName, UidRequestType.FILE),
+    uid: nodeUid,
     name: displayName,
     createdAt: today,
     editedAt: today,
-    type: 'node',
+    type: nodeType,
     supertags: supertags.length > 0 ? supertags : undefined,
   };
+}
+
+function dateStringToDateUID(displayName: string, dateFormat: string): string
+{
+  let date= moment(displayName, dateFormat);
+  if (date.isValid()){
+    return date.format('MM-DD-YYYY');
+  }
+  return '';
 }
